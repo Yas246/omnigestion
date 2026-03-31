7e86c126cad70d29c985507e9922ac3eedd5be95
Yas246 <bigwabi01@gmail.com>
2026-03-31 21:20:51 +0100

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -2,8 +2,8 @@
 // Cache des assets statiques uniquement - PAS de cache Firestore/API
 // v8: Support PWA offline avec persistance localStorage des stores Zustand
 
-const CACHE_NAME = "omnigestion-v20";
-const STATIC_CACHE = "omnigestion-static-v20";
+const CACHE_NAME = "omnigestion-v21";
+const STATIC_CACHE = "omnigestion-static-v21";
 
 // Assets à mettre en cache statique (pages, JS, CSS, images)
 const STATIC_ASSETS = [
